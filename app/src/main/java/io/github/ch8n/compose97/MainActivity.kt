f28d69f88acfabad20e729840a53ca46fedd94ba
ch8n <chetan.garg36@gmail.com>
2021-09-07 23:21:32 +0530

    🏗 wip home screen - added start button

--- app/src/main/java/io/github/ch8n/compose97/MainActivity.kt
+++ app/src/main/java/io/github/ch8n/compose97/MainActivity.kt
@@ -3,21 +3,73 @@ package io.github.ch8n.compose97
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import io.github.ch8n.compose97.ui.theme.Compose97Theme
+import androidx.compose.ui.unit.dp
+import io.github.ch8n.compose97.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Compose97Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    color = Teal,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(0.95f)
+                        ) {
+                            Text(text = "Hello 1")
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(55.dp)
+                                .background(Silver)
+                        ) {
+
+                            Button(
+                                onClick = {},
+                                modifier = Modifier
+                                    .border(
+                                        width = 1.dp,
+                                        color = Black
+                                    )
+                                    .shadow(
+                                        elevation = 2.dp
+                                    )
+                            ) {
+                                Row {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_windows95),
+                                        contentDescription = "start button"
+                                    )
+                                    Spacer(modifier = Modifier.width(6.dp))
+                                    Text(
+                                        text = "Start",
+                                        style = MaterialTheme.typography.button
+                                    )
+                                }
+                            }
+
+                        }
+                    }
                 }
             }
         }
