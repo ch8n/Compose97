ae18d821c362091e18329d84c11140d93d4147f0
ch8n <chetan.garg36@gmail.com>
2021-09-20 23:55:28 +0530

    🏗 refactored code - added window scaffold

--- app/src/main/java/io/github/ch8n/compose97/ui/components/mycomputer/MyComputer.kt
+++ app/src/main/java/io/github/ch8n/compose97/ui/components/mycomputer/MyComputer.kt
@@ -1,14 +1,9 @@
 package io.github.ch8n.compose97.ui.components.mycomputer
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.IdRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,14 +13,12 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import io.github.ch8n.compose97.R
 import io.github.ch8n.compose97.ui.theme.Black
 import io.github.ch8n.compose97.ui.theme.Gray
